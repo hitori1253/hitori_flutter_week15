cf511ee99a8e13f5f1e6082d9b12ff6cfd9bd159
hitori1253 <jont2514@gmail.com>
2026-01-30 11:40:56 +0700

    ้ๆๆๆ

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,17 @@
 import 'package:flutter/material.dart';
-import 'pages/home.dart';
-import 'pages/list.dart';
-import 'pages/grid.dart';
-import 'pages/card.dart';
-import 'pages/second.dart';
+import 'package:hitori15/page/row_page.dart';
+import 'package:hitori15/views/column.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const HomePage(),
-        '/list': (context) => const ListPage(),
-        '/grid': (context) => const GridPage(),
-        '/card': (context) => const CardPage(),
-        '/second': (context) => const SecondPage(),
-      },
-    );
+    return MaterialApp(home: RowPage());
+    // ignore: dead_code
+    return MaterialApp(home: ColumnPage());
   }
 }
